d3582536517298c6f631baab6d2bb16d1934b1c8
Blackfate <blackfate.myfate@gmail.com>
2021-12-09 15:04:03 +0700

    product

--- lib/models/Product.dart
+++ lib/models/Product.dart
@@ -12,36 +12,47 @@ enum KategoriProduct {
 
 class Product {
   //tambah stock?
-  final KategoriProduct kategoriProduct;
-  final String id, nama, photoName, deskripsi;
-  final num harga, stock;
-  final num promo;
-  String? recipeId = '';
-  List<String> bahan = [], langkah = [];
+  final KategoriProduct kategoriProduct, kategoriJasa;
+  final String id, namapr, photoNamepr, deskripsipr, namajs, photoNamejs, deskripsijs;
+  final num hargapr, stockpr, hargajs;
+  final num promo, promojs;
+  String? jasaId = '';
 
   Product({
     required this.id,
-    required this.nama,
+    required this.namapr,
     required this.kategoriProduct,
-    required this.deskripsi,
-    required this.photoName,
-    required this.harga,
+    required this.deskripsipr,
+    required this.photoNamepr,
+    required this.hargapr,
     required this.promo,
-    required this.stock,
-    this.recipeId,
+    required this.stockpr,
+    required this.namajs,
+    required this.kategoriJasa,
+    required this.deskripsijs,
+    required this.photoNamejs,
+    required this.hargajs,
+    required this.promojs,
+    this.jasaId,
   });
 
   factory Product.fromJSON(Map<String, dynamic> map) {
     return Product(
       id: map['id'],
-      nama: map['nama'],
-      deskripsi: map['deskripsi'],
-      harga: map['harga'],
-      photoName: map['photo_name'],
-      kategoriProduct: stringToKategori(map['category']),
+      namapr: map['nama_pr'],
+      deskripsipr: map['deskripsi_pr'],
+      hargapr: map['harga_pr'],
+      photoNamepr: map['photo_name'],
+      kategoriProduct: stringToKategori(map['category_pr']),
       promo: map['promo'],
-      stock: map['stock'],
-      recipeId: map['resep_id'],
+      stockpr: map['stock_pr'],
+      jasaId: map['jasa_id'],
+      namajs: map['nama_js'],
+      deskripsijs: map['deskripsi_js'],
+      hargajs: map['harga_js'],
+      photoNamejs: map['photo_js'],
+      kategoriJasa: stringToKategori(map['category_js']),
+      promojs: map['promo_js'],
     );
   }
 
